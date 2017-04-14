b24f3d0e38e176b70c81781260c0d149668fe380
Stephanie Larocque <larste02@MacBookAir01.local>
2017-04-14 11:12:21 -0400

    first commit

---
+++ autoencoder_model.py
@@ -0,0 +1,96 @@
+import lasagne
+import theano
+import theano.tensor as T
+import numpy as np
+
+#Import layers from lasagne
+from lasagne.layers import InputLayer, DropoutLayer, ReshapeLayer
+from lasagne.layers import BatchNormLayer, batch_norm
+from lasagne.layers import NonlinearityLayer, DimshuffleLayer, ConcatLayer
+from lasagne.layers import Layer
+from lasagne.layers import Conv2DLayer
+from lasagne.layers import Deconv2DLayer #same as TransposeConv2DLayer
+from lasagne.layers import Pool2DLayer
+from lasagne.layers import Upscale2DLayer
+from lasagne.layers import DenseLayer
+
+#Import nonlinearities
+from lasagne.nonlinearities import rectify
+from lasagne.nonlinearities import softmax
+from lasagne.nonlinearities import sigmoid
+
+
+class Model(object):
+    def __init__(self):
+        pass
+
+    def build_network(self):
+        raise NotImplementedError
+
+
+class AE(Model):
+	def __init__(self):
+		pass
+	
+	def build_network(self, input_var,
+		conv_before_pool=[2,2],
+		n_filters = 32,
+		code_size = 100,
+		filter_size = 3,
+		pool_factor = 2
+		):
+		
+		self.conv_before_pool = conv_before_pool
+		self.code_size = code_size
+		self.n_filters = n_filters
+		n_block = len(conv_before_pool)
+		
+		net={}
+		net['input'] = InputLayer((None, 3, 32, 32), input_var)
+		incoming_layer = 'input'
+		
+		#Encoder (Conv + Pooling Layers)
+		for i in range(n_block):
+			n_conv = conv_before_pool[i]
+		
+			for c in range(n_conv): #c-th convolution for the i-th block
+				net['conv'+str(i)+'_'+str(c)] = Conv2DLayer(net[incoming_layer],
+					#n_filters increases as size of image decreases
+					num_filters = n_filters * pool_factor**i,
+					filter_size = filter_size,
+					nonlinearity = rectify,
+					pad='same')
+                incoming_layer = 'conv'+str(i)+'_'+str(c)
+                
+                
+		net['pool'+str(i)] = Pool2DLayer(net[incoming_layer], pool_size = pool_factor)
+		incoming_layer = 'pool'+str(i)
+            
+        	#Encoder (Reshape + Dense Layer)
+        net['reshape_enc'] = ReshapeLayer(net[incoming_layer],
+        	shape=([0],-1))
+        incoming_layer = 'reshape_enc'
+        
+        
+        
+        
+        #Code 
+        #Decoder (Dense + Reshape Layer)
+        #Decoder (Upscaling + Conv Layers)
+       
+       
+       
+            
+if __name__ == '__main__':
+	input_var = T.tensor4('input')
+	AE.buid_network(input_var)
+	
+		
+		
+	
+	
+	
+	
+	
+	
+	
